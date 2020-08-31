c9b77c1f3f7fbc2285d9b363bced18b801f46246
Nadine <45455716+nadinev6@users.noreply.github.com>
2020-08-31 22:48:35 +0200

    Create win-vm.sh
    Windows-VM prompt for variables:
    $VM-NAME
    $REGION
    $ZONE
    $MACHINE TYPE
    $IMAGE
    $IMAGE-PROJECT
    $BOOT-DISK-SIZE
    $BOOT-DISK-TYPE
    $TAGS

---
+++ win-vm.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Enter VM name:
+echo Name:
+read VM-NAME
+echo VM-NAME=$VM-NAME
+
+# Enter Region:
+echo Region:
+read REGION
+echo REGION=$REGION
+
+# Enter Zone:
+echo Zone:
+read ZONE
+echo ZONE=$ZONE
+
+# Enter Machine type:
+echo Machine type:
+read MACHINE TYPE
+echo MACHINE TYPE=$MACHINE TYPE
+
+# Enter Machine type:
+echo Boot disk:
+read IMAGE
+echo IMAGE=$IMAGE
+
+# Enter Machine type:
+echo Boot disk:
+read IMAGE
+echo IMAGE=$IMAGE
+
+# Enter Operating System:
+echo Operating System:
+read IMAGE-PROJECT
+echo IMAGE-PROJECT=$IMAGE-PROJECT
+
+# Enter Boot disk size:
+echo Boot disk size:
+read BOOT-DISK-SIZE
+echo BOOT-DISK-SIZE=$BOOT-DISK-SIZE
+
+# Enter Boot disk type:
+echo Boot disk size:
+read BOOT-DISK-TYPE
+echo BOOT-DISK-TYPE=$BOOT-DISK-TYPE
+
+# Allow Firewall rules:
+echo Enter tags:
+read TAGS
+echo TAGES=$TAGS
